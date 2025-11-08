0e8291feb3ec4985d6c592c3a41f8c09457b88bf
Honorine Ndom Ndzah <87605893+Honorinenn@users.noreply.github.com>
2025-11-08 21:17:41 +0000

    added agents

---
+++ ManufacturingAgents/supervisory_agent/sub_agents/logistics_agent/agent.py
@@ -0,0 +1,12 @@
+from google.adk.agents import Agent
+
+root_agent = Agent(
+    name="logistics_agent",
+    # https://ai.google.dev/gemini-api/docs/models
+    model="gemini-2.0-flash",
+    description="Logistics agent",
+    instruction="""
+    You are a helpful assistant that plans production in a manufacturing plant. 
+    Ask for the input and return the output.
+    """,
+)
